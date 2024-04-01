53283a761769407a0b50e6fc6da6c1acc5ea88a1
Andrew <andrew.ndarua@student.moringaschool.com>
2024-04-01 23:57:22 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+{let companyName = 'Scuber'; 
+}
+
+{const companyName = 'companyName'; 
+}
+
+{let mostProfitableNeighborhood = 'Chelsea';
+} 
+
+{const mostProfitableNeighborhood = "mostProfitableNeighborhood";
+}
+
+{let companyCeo ='Susan Smith'
+}
+
+{const companyCeo = 'companyCeo'
+}
